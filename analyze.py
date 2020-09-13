54f4eba15401b2322086fb4e393904631a42af44
mriyami <mriyami.1998@gmail.com>
2020-09-13 21:43:35 +0400

    Creat and plot PDF (task 2)
    added script that plots PDF of all sensor data from "Lab1" into one figure with 3 sub-plots

--- analyze.py
+++ analyze.py
@@ -16,6 +16,7 @@ from datetime import datetime
 import typing as T
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
 
 def load_data(file: Path) -> T.Dict[str, pandas.DataFrame]:
@@ -67,18 +68,38 @@ if __name__ == "__main__":
     print(data['temperature'].var())
     #print("Median occupancy for each room\n" + data['occupancy'].median())
     #print("Variance in occupancy for each rooms\n" + data['occupancy'].var() + "\n\n")
-
-
-
-
-
+"""
     for k in data:
-        #data[k].plot()
-        #time = data[k].index
+        data[k].plot()
+        time = data[k].index
         data[k].hist()
-        #plt.figure()
-        #plt.hist(np.diff(time.values).astype(np.int64) // 1000000000)
-        #plt.xlabel("Time (seconds)")
+        plt.figure()
+        plt.hist(np.diff(time.values).astype(np.int64) // 1000000000)
+        plt.xlabel("Time (seconds)")
+"""
+
+#one of many failed attempts at plotting PDF
+"""
+#data['temperature']['lab1'].hist(bins=3000, density= True)
+#n, x, _ = plt.hist(data['temperature']['lab1'], bins=np.linspace(-300, 50, 5), 
+#          histtype=u'step', density=True) 
+#sns.set_style('whitegrid')
+#sns.kdeplot(np.array(data['temperature']['lab1']))
+#sns.displot(np.array(data['temperature']['lab1']),range = (-50,50), kde=True)
+#g = sns.FacetGrid(data['temperature'], col = 3)
+"""
+#Creat and plot PDF:
+fig, axs = plt.subplots(1, 3,figsize=(9,6))
+fig.suptitle('Propability Density', fontsize=16)
+sns.histplot(np.array(data['temperature']['lab1']), stat = 'density', ax = axs[0], kde = True)
+sns.histplot(np.array(data['occupancy']['lab1']), stat = 'density', discrete = True, ax = axs[1], kde = True)
+sns.histplot(np.array(data['co2']['lab1']), stat = 'density', ax = axs[2], kde = True)
+axs[1].set_ylabel('')
+axs[2].set_ylabel('')
+axs[0].set_xlabel('temperature')
+axs[1].set_xlabel('occupancy')
+axs[2].set_xlabel('co2')
+plt.tight_layout() 
 
 
-    plt.show()
+plt.show()
